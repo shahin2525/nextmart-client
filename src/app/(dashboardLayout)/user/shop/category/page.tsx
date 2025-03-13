2351f244c622f526594d134e25065558afa3d5bb
shahin2525 <shahin451999@gmail.com>
2025-03-13 10:39:52 +0530

    add get all category service

--- src/app/(dashboardLayout)/user/shop/category/page.tsx
+++ src/app/(dashboardLayout)/user/shop/category/page.tsx
@@ -1,7 +1,10 @@
 import ManageCategories from "@/components/modules/shop/category";
+import { getAllCategory } from "@/services/category";
 import React from "react";
 
-const ProductCategory = () => {
+const ProductCategory = async () => {
+  const allCategory = await getAllCategory();
+  console.log(allCategory);
   return (
     <div>
       <ManageCategories />
--- src/services/category/index.ts
+++ src/services/category/index.ts
@@ -4,34 +4,26 @@ import { cookies } from "next/headers";
 
 export const createCategory = async (data: FormData) => {
   try {
-    const res = await fetch("http://localhost:5000/api/v1/category", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/category`, {
       method: "POST",
       headers: {
         Authorization: (await cookies()).get("accessToken")!.value,
       },
       body: data,
     });
-    console.log("from server", res);
+
     return res.json();
   } catch (error: any) {
     return Error(error);
   }
 };
 
-// "use server";
-// import { cookies } from "next/headers";
+export const getAllCategory = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/category`);
 
-// export const createCategory = async (data: FormData) => {
-//   try {
-//     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/category`, {
-//       method: "POST",
-//       headers: {
-//         Authorization: (await cookies()).get("accessToken")!.value,
-//       },
-//       body: data,
-//     });
-//     return res.json();
-//   } catch (err: any) {
-//     console.error(err);
-//   }
-// };
+    return res.json();
+  } catch (error: any) {
+    return Error(error);
+  }
+};
