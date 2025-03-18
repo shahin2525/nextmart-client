faeaac90fb5f89d563498b08c1a9e2612d926b42
shahin2525 <shahin451999@gmail.com>
2025-03-18 16:16:36 +0530

    add all product banner

--- src/app/(commonLayout)/products/page.tsx
+++ src/app/(commonLayout)/products/page.tsx
@@ -1,12 +1,24 @@
+import AllProducts from "@/components/modules/products";
 import AllProductsBanner from "@/components/modules/products/banner";
+import CategoryCard from "@/components/ui/core/CategoryCard";
 import NMContainer from "@/components/ui/core/NMContainer";
+import { getAllCategory } from "@/services/category";
+import { ICategory } from "@/types";
 
-const ProductsPage = () => {
+const AllProductsPage = async () => {
+  const { data: categories } = await getAllCategory();
+
   return (
     <NMContainer>
       <AllProductsBanner title="All Product" path="Home Products" />
+      <div className="grid grid-cols-6 gap-8 my-5">
+        {categories?.slice(0, 6).map((category: ICategory, idx: number) => (
+          <CategoryCard key={idx} category={category} />
+        ))}
+      </div>
+      <AllProducts />
     </NMContainer>
   );
 };
 
-export default ProductsPage;
+export default AllProductsPage;
---
+++ src/components/modules/products/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AllProducts = () => {
+  return (
+    <div className="flex">
+      <div>Sidebar</div>
+      <div>All products</div>
+    </div>
+  );
+};
+
+export default AllProducts;
