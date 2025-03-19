442bcacaa1ced4811bdb9bdf4c2c410d6ba8cbf1
shahin2525 <shahin451999@gmail.com>
2025-03-19 22:27:08 +0530

    add coupon

---
+++ src/app/(commonLayout)/cart/page.tsx
@@ -0,0 +1,18 @@
+import CartProducts from "@/components/modules/cart/CartProduct";
+import Coupon from "@/components/modules/cart/Coupon";
+import AllProductsBanner from "@/components/modules/products/banner";
+import NMContainer from "@/components/ui/core/NMContainer";
+
+const CartPage = () => {
+  return (
+    <NMContainer>
+      <AllProductsBanner title="Cart page" path="home cart page" />
+      <div className="grid grid-cols-12 gap-8 my-5">
+        <CartProducts />
+        <Coupon />
+      </div>
+    </NMContainer>
+  );
+};
+
+export default CartPage;
